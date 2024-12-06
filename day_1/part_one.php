bafb9fedd6b5279fa7cd1992af623b2210144361
Bianca Sales <bianca.sales@ebanx.com>
2024-12-06 15:36:19 -0300

    day one challenges

--- day_1/part_one.php
+++ day_1/part_one.php
@@ -1,6 +1,24 @@
 <?php
 
-class part_one
-{
+$filename = 'numbers_day_one.txt';
+$file_contents = file_get_contents($filename);
 
+$diff = 0;
+$lines = array_map('trim', explode("\n", $file_contents));
+$right_side = [];
+$left_side = [];
+
+foreach ($lines as $line) {
+	list($right, $left) = explode("   ", $line);
+	$right_side[] = (int)$right;
+	$left_side[] = (int)$left;
 }
+
+sort($right_side);
+sort($left_side);
+
+foreach ($right_side as $key => $value) {
+	$diff += abs($value - $left_side[$key]);
+}
+
+print_r($diff);
--- day_1/part_two.php
+++ day_1/part_two.php
@@ -1,6 +1,25 @@
 <?php
 
-class part_two
-{
+$filename = 'numbers_day_one.txt';
+$file_contents = file_get_contents($filename);
 
+$lines = array_map('trim', explode("\n", $file_contents));
+$right_side = [];
+$left_side = [];
+$total_similarity = 0;
+foreach ($lines as $line) {
+	list($right, $left) = explode("   ", $line);
+	$right_side[] = (int)$right;
+	$left_side[] = (int)$left;
 }
+
+$left_side_count_values = array_count_values($left_side);
+
+foreach ($right_side as $key => $value) {
+	if (array_key_exists($value, $left_side_count_values)) {
+		$total_similarity += ($value * $left_side_count_values[$value]);
+	}
+}
+
+print_r($total_similarity);
+
